a9cd701e3294276b64cc8606b194161af426d8b6
howdy39 <tatsuya.howdy@gmail.com>
2018-05-29 22:27:34 +0900

    fix: コアモジュールを作成

---
+++ lesson55/src/app/core/core.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
+@NgModule({
+  imports: [
+    AngularFireAuthModule,
+    AngularFirestoreModule
+  ],
+  declarations: []
+})
+export class CoreModule { }
